eaa497fbe4e007e48e67c2bc20025b361cb25bc4
Poom <poompongphun@gmail.com>
2022-05-25 18:38:32 +0700

    add growSection, navbarEffect

--- main.js
+++ main.js
@@ -1,6 +1,49 @@
 const doc = document.documentElement;
 const darkBtn = document.getElementById("darkBtn");
 const svg = document.getElementsByClassName("svg");
+const section = doc.querySelectorAll("section");
+const navMenu = document.getElementById("navMenu");
+const navA = navMenu.querySelectorAll("a");
+let currentSection = "home";
+
+navA.forEach((a) => {
+  a.onclick = () => {
+    activeBtn(a.href.split("#")[1]);
+  };
+});
+
+window.onscroll = () => {
+  if (document.documentElement.scrollTop == 0) {
+    document.getElementsByClassName("navbar")[0].style.height = "86px";
+  } else document.getElementsByClassName("navbar")[0].style.height = "56px";
+  let top = doc.scrollTop;
+  let bottom = top + doc.offsetHeight;
+  let arr = [];
+
+  section.forEach((ele) => {
+    if (
+      (ele.offsetTop < top && top < ele.offsetTop + ele.offsetHeight) ||
+      (ele.offsetTop < bottom && bottom < ele.offsetTop + ele.offsetHeight)
+    ) {
+      arr.push(ele.id);
+    }
+  });
+  if (arr[0] && arr[0] != currentSection) {
+    currentSection = arr[0];
+    activeBtn(currentSection);
+  }
+};
+
+function activeBtn(id) {
+  navA.forEach((aa) => {
+    if (aa.classList.contains("active")) {
+      aa.classList.remove("active");
+    } else if (aa.href.split("#")[1] == id) {
+      aa.classList.add("active");
+    }
+  });
+}
+
 function swDark(e) {
   //   this.dataset.dark = !this.dataset.dark;
   const value = e.dataset.dark === "true";
@@ -15,6 +58,7 @@ function swDark(e) {
     doc.style.setProperty("--normal", "hsl(228, 7%, 13%)");
     doc.style.setProperty("--btnColor", "hsl(0, 0%, 100%)");
     doc.style.setProperty("--bl", "black");
+    doc.style.setProperty("--navbar", "hsla(198, 74%, 82%, 0.5)");
   } else {
     // ดำ
     doc.style.setProperty("--bgColorST", "hsl(231, 30%, 13%)");
@@ -24,6 +68,7 @@ function swDark(e) {
     doc.style.setProperty("--normal", "hsl(231, 24%, 72%)");
     doc.style.setProperty("--btnColor", "hsl(229, 32%, 17%)");
     doc.style.setProperty("--bl", "white");
+    doc.style.setProperty("--navbar", "hsla(232, 38%, 11%, 0.5)");
   }
   for (let i = 0; i < svg.length; i++) {
     const ele = svg[i];
